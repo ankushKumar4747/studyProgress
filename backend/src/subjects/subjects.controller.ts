6f08869ac05ca191576a82ab17990c1f18f9312c
Anonimus Studio's <ankushbhagay4747@gmail.com>
2026-01-02 12:57:05 +0530

    try catch is added into subjects

--- backend/src/subjects/subjects.controller.ts
+++ backend/src/subjects/subjects.controller.ts
@@ -20,26 +20,54 @@ export class SubjectsController {
   constructor(private readonly subjectsService: SubjectsService) {}
   @Post('studyGoal')
   async studyGoal(@Body() body: { min: number }, @Req() req: any) {
-    const userId = req.user.id;
-    return this.subjectsService.studyGoal(userId, body.min);
+    try {
+      const userId = req.user.id;
+      return this.subjectsService.studyGoal(userId, body.min);
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Internal Server Error',
+      };
+    }
   }
 
   @Get('studyGoal')
   async getStudyGoal(@Req() req: any) {
-    const userId = req.user.id;
-    return this.subjectsService.getStudyGoal(userId);
+    try {
+      const userId = req.user.id;
+      return this.subjectsService.getStudyGoal(userId);
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Internal Server Error',
+      };
+    }
   }
 
   @Get('streak')
   async getStreak(@Req() req: any) {
-    const userId = req.user.id;
-    return this.subjectsService.getStreak(userId);
+    try {
+      const userId = req.user.id;
+      return this.subjectsService.getStreak(userId);
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Internal Server Error',
+      };
+    }
   }
 
   @Get('list')
   async getUserSubjects(@Req() req: any) {
-    const userId = req.user.id;
-    return this.subjectsService.getUserSubjects(userId);
+    try {
+      const userId = req.user.id;
+      return this.subjectsService.getUserSubjects(userId);
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Internal Server Error',
+      };
+    }
   }
 
   @Get(':subjectId/progress')
@@ -47,53 +75,105 @@ export class SubjectsController {
     @Param('subjectId') subjectId: string,
     @Req() req: any,
   ) {
-    const userId = req.user.id;
-    return this.subjectsService.getSubjectProgress(subjectId, userId);
+    try {
+      const userId = req.user.id;
+      return this.subjectsService.getSubjectProgress(subjectId, userId);
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Internal Server Error',
+      };
+    }
   }
 
   @Get('totalSubjectsWithData')
   async totalSubjectsWithData(@Req() req: any) {
-    const userId = req.user.id;
-    return this.subjectsService.totalSubjectsWithData(userId);
-  } 
+    try {
+      const userId = req.user.id;
+      return this.subjectsService.totalSubjectsWithData(userId);
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Internal Server Error',
+      };
+    }
+  }
 
   @Post('studyTimeUpdate')
   async updateStudyTime(
     @Body()
-    studyTime: { min: number; subjectId: mongoose.Schema.Types.ObjectId, numberOfCompletedTopics: number },
+    studyTime: {
+      min: number;
+      subjectId: mongoose.Schema.Types.ObjectId;
+      numberOfCompletedTopics: number;
+    },
     @Req() req: any,
   ) {
-    const userId = req.user.id;
-    return this.subjectsService.updateStudyTime(userId, studyTime);
+    try {
+      const userId = req.user.id;
+      return this.subjectsService.updateStudyTime(userId, studyTime);
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Internal Server Error',
+      };
+    }
   }
 
   @Get('studyTime')
   async getStudyTime(@Req() req: any) {
-    const userId = req.user.id;
-    return this.subjectsService.getTodayStudyTime(userId);
+    try {
+      const userId = req.user.id;
+      return this.subjectsService.getTodayStudyTime(userId);
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Internal Server Error',
+      };
+    }
   }
 
   @Get('weekly-focus')
   async getWeeklyFocusDistribution(@Req() req: any) {
-    const userId = req.user.id;
-    return this.subjectsService.getWeeklyFocusDistribution(userId);
+    try {
+      const userId = req.user.id;
+      return this.subjectsService.getWeeklyFocusDistribution(userId);
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Internal Server Error',
+      };
+    }
   }
 
   @Get('weekly-mastery')
   async getWeeklyMastery(@Req() req: any) {
-    const userId = req.user.id;
-    return this.subjectsService.getWeeklyMastery(userId);
+    try {
+      const userId = req.user.id;
+      return this.subjectsService.getWeeklyMastery(userId);
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Internal Server Error',
+      };
+    }
   }
-
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async handleStreakUpdate() {
-   this.subjectsService.handleStreakUpdate();
+    this.subjectsService.handleStreakUpdate();
   }
 
   @Post('updateCompletedTopics')
-  async updateCompletedTopics(@Req() req: any, @Body() subjectData: any){
-    const userId = req.user.id;
-    return this.subjectsService.updateCompletedTopics(userId,subjectData);
+  async updateCompletedTopics(@Req() req: any, @Body() subjectData: any) {
+    try {
+      const userId = req.user.id;
+      return this.subjectsService.updateCompletedTopics(userId, subjectData);
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Internal Server Error',
+      };
+    }
   }
 }
